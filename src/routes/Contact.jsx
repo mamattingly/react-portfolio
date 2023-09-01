f6684404ed8847ac1ecb44be2302b0fd591a5170
Michael Mattingly <mikeamatt1@hotmail.com>
2023-09-01 00:38:40 -0400

    added Contact Me Form

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,7 +1,42 @@
-const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./ContactStyles.css";
 
-export default Contact
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.VITE_SERVICE,
+        import.meta.VITE_TEMPLATE,
+        form.current,
+        import.meta.VITE_PK
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="contact-form">
+      <h2>Contact Me</h2>
+      <label>Name</label>
+      <input type="text" name="user_name" required />
+      <label>Email</label>
+      <input type="email" name="user_email" required />
+      <label>Message</label>
+      <textarea name="message" required />
+      <input type="submit" value="Send" className="submit-btn" /> 
+    </form>
+  );
+};
+
+export default Contact;
