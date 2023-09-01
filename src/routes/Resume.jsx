f13f8d6fc62cd0fe5c50ab3a0e3bab088f5dac08
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-31 23:11:36 -0400

    added resume page and styles

--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -1,9 +1,46 @@
-
+import "./ResumeStyles.css";
 
 const Resume = () => {
-  return (
-    <div>Resume</div>
-  )
-}
+  const pdfUrl = "../src/assets/documents/resume.pdf";
 
-export default Resume
+  return (
+    <div className="resume-container">
+      <h2 className="title">Click Below to Download</h2>
+      <a href={pdfUrl} download className="download-link">
+        Download Resume
+      </a>
+      <div className="skills-container">
+        <h3>Skills</h3>
+        <br />
+        <ul className="skills-list">
+          <li>HTML5</li>
+          <li>CSS</li>
+          <li>ES6 JavaScript</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>Bootstrap</li>
+          <li>Animate.css</li>
+          <li>Tailwind</li>
+          <li>Java</li>
+          <li>Python</li>
+          <li>Oracle/MySQL</li>
+          <li>MongoDB</li>
+          <li>Jest</li>
+          <li>jQuery</li>
+          <li>AJAX</li>
+          <li>React</li>
+          <li>Web API</li>
+          <li>Git/GitHub</li>
+          <li>REST API</li>
+          <li>JSON</li>
+          <li>Mongoose</li>
+          <li>React-Router</li>
+          <li>React-Redux</li>
+          <li>Graph Ql</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
