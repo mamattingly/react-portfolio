5fcd4728f4189a78941c99ebb19439a4f496c7f3
Michael Mattingly <mikeamatt1@hotmail.com>
2024-02-02 00:54:34 -0500

    Updated Resume and Added new link.

--- src/routes/Resume/Resume.jsx
+++ src/routes/Resume/Resume.jsx
@@ -1,5 +1,5 @@
 import "./ResumeStyles.css";
-import resume from "../../assets/documents/resume.pdf";
+import resume from "../../assets/documents/resume2024.pdf";
 
 // Resume renders a link to download my resume and a list of my skills.
 const Resume = () => {
